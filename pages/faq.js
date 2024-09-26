1d22268023ac5629924a43ebbf0418bde69c8dfd
Lucas Vasconcelos <lucasvchaves22@gmail.com>
2024-09-25 22:05:20 -0300

    inserindo a funcionalidade Link do Next

---
+++ pages/faq.js
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+
+export default function FAQPage() {
+    return (
+        <div>
+            <h1>Alura Cases - Página de Perguntas FAQ</h1>
+            <Link href="/">
+                Ir para a home
+            </Link>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
+import Link from "next/link"
+
+
 export default function HomePage() {
     return (
         <div>
             <h1>Alura Cases - Home</h1>
+            <Link href="/faq">
+                Ir para o FAQ
+            </Link>
         </div>
     )
 }
